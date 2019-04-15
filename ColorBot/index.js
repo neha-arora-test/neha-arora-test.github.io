0d1f46fe57a52ed938ff80bf20d7b4c759c75608
neha-arora-test <49002999+neha-arora-test@users.noreply.github.com>
2019-04-15 06:11:52 -0400

    update

--- ColorBot/index.js
+++ ColorBot/index.js
@@ -129,7 +129,7 @@ class Classifier {
     const inputText =
         text.trim().toLowerCase().replace(/(\.|\,|\!)/g, '').split(' ');
     // Look up word indices.
-    const inputBuffer = tf.buffer([0, this.maxLen], 'float32');
+    const inputBuffer = tf.buffer([1, this.maxLen], 'float32');
     for (let i = 0; i < inputText.length; ++i) {
       const word = inputText[i];
       inputBuffer.set(this.wordIndex[word], 0, i);
